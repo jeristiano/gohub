e24dafbad71342c1a5d53fb24f20d68369376bfb
kuangzhenyong <kuangzhenyong@zhaoshang.net>
2022-07-13 19:36:06 +0800

    手机是否注册接口

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -16,18 +16,22 @@ type SignupController struct {
 }
 
 func (sc *SignupController) IsPhoneExist(c *gin.Context) {
+
 	// 请求对象
 	type PhoneExistRequest struct {
 		Phone string `json:"phone"`
 	}
 	request := PhoneExistRequest{}
 
-	if err := c.ShouldBindJSON(&request); err == nil {
+	if err := c.ShouldBindJSON(&request); err != nil {
 		// 解析失败，返回 422 状态码和错误信息
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"error": err.Error(),
+		})
 
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
-
+		// 打印错误信息
 		fmt.Println(err.Error())
+		// 出错了，中断请求
 		return
 	}
 	//  检查数据库并返回响应
