4707f3674afd5d6e62fe447126761933970f6cb3
jersitiano <jeremykuang@foxmail.com>
2022-10-25 20:15:18 +0800

    话题列表"

--- routes/api.go
+++ routes/api.go
@@ -71,6 +71,7 @@ func RegisterAPIRoutes(r *gin.Engine) {
 					tpcGroup.POST("", middlewares.AuthJWT(), tpc.Store)
 					tpcGroup.POST("", middlewares.AuthJWT(), tpc.Store)
 					tpcGroup.PUT("/:id", middlewares.AuthJWT(), tpc.Update)
+					tpcGroup.DELETE("/:id", middlewares.AuthJWT(), tpc.Delete)
 				}
 			}
 		}
