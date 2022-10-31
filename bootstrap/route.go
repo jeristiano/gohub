dbc35e80f703ae87c384648b25ec001745ba2804
jersitiano <jeremykuang@foxmail.com>
2022-10-31 20:30:47 +0800

    初始化路由

---
+++ bootstrap/route.go
@@ -0,0 +1,42 @@
+// Package bootstrap 处理程序初始化逻辑
+package bootstrap
+
+import (
+	"net/http"
+	"strings"
+
+	"golanghub/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoute(router *gin.Engine) {
+	// 注册全局中间件
+	registerGlobalMiddleWare(router)
+
+	//  注册 API 路由
+	routes.RegisterAPIRoutes(router)
+
+	//  配置 404 路由
+	setup404Handler(router)
+
+}
+
+func registerGlobalMiddleWare(router *gin.Engine) {
+	router.Use(gin.Logger(), gin.Recovery())
+}
+
+func setup404Handler(router *gin.Engine) {
+	router.NoRoute(func(c *gin.Context) {
+		// 获取标头信息的 Accept 信息
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			c.String(http.StatusNotFound, "页面返回 404")
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
+			})
+		}
+	})
+}
--- main.go
+++ main.go
@@ -1,50 +1,23 @@
 package main
 
 import (
-	"net/http"
-	"strings"
+	"fmt"
+	"golanghub/bootstrap"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
-	// r := gin.Default()
-	// r.GET("/", func(ctx *gin.Context) {
-	// 	// 注册一个路由
-	// 	// 以 JSON 格式响应
-	// 	ctx.JSON(http.StatusOK, gin.H{"Hello": "World!"})
-	// })
-
-	// r.Run()
 	// new 一个 Gin Engine 实例
-	r := gin.New()
+	router := gin.New()
 
-	// 注册中间件
-	r.Use(gin.Logger(), gin.Recovery())
-	// 注册一个路由
-	r.GET("/", func(c *gin.Context) {
-
-		// 以 JSON 格式响应
-		c.JSON(http.StatusOK, gin.H{
-			"Hello": "World!",
-		})
-	})
-
-	r.NoRoute(func(c *gin.Context) {
-		// 获取标头信息的 Accept 信息
-		acceptString := c.Request.Header.Get("Accept")
-		if strings.Contains(acceptString, "text/html") {
-			c.String(http.StatusNotFound, "页面返回 404")
-		} else {
-			c.JSON(http.StatusNotFound, gin.H{
-				"error_code":    404,
-				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
-			})
-		}
-	})
-
-	// 运行服务，默认为 8080，我们指定端口为 8000
-	r.Run(":8000")
+	bootstrap.SetupRoute(router)
+	// 运行服务
+	err := router.Run(":3000")
+	if err != nil {
+		// 错误处理，端口被占用了或者其他错误
+		fmt.Println(err.Error())
+	}
 
 }
